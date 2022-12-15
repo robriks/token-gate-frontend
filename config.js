aeb57069ac4095d6604b3d257aa9f656bc63a46e
robriks <80549215+robriks@users.noreply.github.com>
2022-12-14 19:29:08 -0500

    deprecate rinkeby config
    deprecate rinkeby address in config file, add deployed polygon address

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    rinkeby: '0x402d16921C2E2d1E57D4314E6d3e31Dbe17a090c',
-    polygon: '',
+    // rinkeby is deprecated
+    // rinkeby: '0x402d16921C2E2d1E57D4314E6d3e31Dbe17a090c',
+    polygon: '0x402d16921C2E2d1E57D4314E6d3e31Dbe17a090c',
     mainnet: '',
 }
